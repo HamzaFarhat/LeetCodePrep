19b579ec30689b103c6c562c10eeb3a471d0e188
HamzaFarhat <48735251+HamzaFarhat@users.noreply.github.com>
2022-09-15 01:36:01 -0400

    Create sol.py

---
+++ sol.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+    You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+        """
+#         profit=0
+        
+#         for a in range(0,len(prices)-1):            
+#             diff = prices[a+1] - prices[a]    
+#             if diff>0:
+#                 profit += diff
+                
+#         return profit
